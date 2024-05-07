155c6d552017d30d33d01989a48c6627dfe0ef44
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-05-07 17:26:44 +0100

    first version

--- app.js
+++ app.js
@@ -1,45 +1,76 @@
-const taskInput = document.getElementById("new-task");
-const addTaskBtn = document.getElementById("add-task");
-const taskList = document.getElementById("task-list");
+const todoInput = document.getElementById("new-task");
+const todoBtn = document.getElementById("add-task");
+const todoList = document.getElementById("task-list");
 
 function addTask() {
-    const task = taskInput.value;
+    const task = todoInput.value;
     if(task === '') {
-        alert("please add a task");
-        return;
+        alert("Enter a task");
     }
 
-    const taskItem = document.createElement("li");
-    taskItem.className = "myLi";
-    
-    const taskText = document.createElement("span");
-    taskText.textContent = task;
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "delete";
-    deleteBtn.className = "delete-btn"
-
-    deleteBtn.addEventListener("click", function () {
-        taskList.removeChild(taskItem);
-    })
+    const todoBody = document.createElement('li');
+    const todoText = document.createElement("span");
+    todoText.textContent = task;
 
     const editBtn = document.createElement("button");
     editBtn.textContent = "edit";
-    editBtn.style.backgroundColor = "light-blue";
 
-    editBtn.addEventListener("click", function () {
-        const newTask = prompt("Edit Your Task", taskText.textContent);
-        if(newTask !== null && newTask !== '') {
-            taskText.textContent = newTask;
+    editBtn.addEventListener("click", function() {
+        const newText = prompt("Edit Todo", todoText.textContent);
+        if(newText !== '' && newText !== null) {
+            todoText.textContent = newText;
         }
     })
 
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete";
 
-    taskItem.appendChild(taskText);
-    taskItem.appendChild(editBtn);
-    taskItem.appendChild(deleteBtn);
-    taskList.appendChild(taskItem);
-    taskInput.value = '';
+    deleteBtn.addEventListener("click", function() {
+        todoList.removeChild(todoBody);
+    })
+
+    const completeBtn = document.createElement('button');
+    completeBtn.textContent = "completed";
+
+    completeBtn.addEventListener("click", function () {
+        todoBody.classList.toggle("complete");
+    })
+
+    todoBody.appendChild(todoText);
+    todoBody.appendChild(completeBtn);
+    todoBody.appendChild(editBtn);
+    todoBody.appendChild(deleteBtn);
+    todoList.appendChild(todoBody);
+    todoInput.value = '';
+    
 }
 
-addTaskBtn.addEventListener("click", addTask);
+todoBtn.addEventListener("click", addTask);
+
+
+function filterTodo(status) {
+    const todos = document.getElementsByTagName("li");
+    for(const todo of todos) {
+        switch (status) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "complete":
+                todo.style.display = todo.classList.contains("complete") ? "flex" : "none";
+                break;
+            case "uncomplete":
+                todo.style.display = todo.classList.contains("complete") ? "none" : "flex";
+                break;
+        }
+    }
+}
+
+document.getElementById("all").addEventListener("click", function() {
+    filterTodo("all");
+});
+document.getElementById("comp").addEventListener("click", function() {
+    filterTodo("complete");
+});
+document.getElementById("uncomp").addEventListener("click", function() {
+    filterTodo("uncomplete");
+});
